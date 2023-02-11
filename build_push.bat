ba8fd8578d2fdca29a2e5ebbdbe102cc95982059
Unagi11 <unagi11@naver.com>
2023-02-11 22:30:26 +0900

    build: 2023-02-11 22:30:26

--- build_push.bat
+++ build_push.bat
@@ -1,19 +1,8 @@
-REM Delete the "docs" folder
 rmdir /s /q docs
-
-REM Run the "build" script defined in package.json
-npm run build
-
-REM Copy the "dist" folder to the "docs" folder
+@REM npm run build
 xcopy /E /Y dist docs /I
-
-REM Stage all changes and commit with the current date and time
 git add .
-
-REM YYYY-MM-DD HH:MM:SS
 git commit -m "build: %DATE% %TIME:~0,8%"
-
-REM Push changes to remote repository
 git push
 
 echo Build complete.
