330095f3bcf8158051fd346edc0a8ad47b733fa8
Unagi11 <unagi11@naver.com>
2023-02-11 22:22:59 +0900

    build: 2023-02-11 22:22:59

--- build_push.bat
+++ build_push.bat
@@ -7,10 +7,12 @@ REM Run the "build" script defined in package.json
 npm run build
 
 REM Copy the "dist" folder to the "docs" folder
-xcopy /E /Y dist docs
+xcopy /E /Y dist docs /I
 
 REM Stage all changes and commit with the current date and time
 git add .
+
+REM YYYY-MM-DD HH:MM:SS
 git commit -m "build: %DATE% %TIME:~0,8%"
 
 REM Push changes to remote repository
