4f1ad6b1529c9777c059ac2ebd1b88cf587b618c
Unagi11 <unagi11@naver.com>
2023-02-11 22:17:15 +0900

    build: --0--1 22:17:15

--- build_push.bat
+++ build_push.bat
@@ -11,7 +11,8 @@ xcopy /E /Y dist docs
 
 REM Stage all changes and commit with the current date and time
 git add .
-git commit -m "build: $(date '+%Y-%m-%d %H:%M:%S')"
+set datetime=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+git commit -m "build: %datetime%"
 
 REM Push changes to remote repository
 git push
