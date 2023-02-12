ad0351a7e1bd6cdc5e42f697f4d89aa3761f145c
Unagi11 <unagi11@naver.com>
2023-02-12 17:12:43 +0900

    build: 2023-02-12 17:12:43

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import frag from './shaders/fragment.frag';
-import vert from './shaders/vertex.vert';
-import { Application, Sprite, Container, Filter } from 'pixi.js';
-export const global : any = window as any;
+import frag from './shaders/fragment.frag'
+import vert from './shaders/vertex.vert'
+import { Application, Sprite, Container, Filter, Point, Graphics, Text, TextStyle } from 'pixi.js'
+
+export const global: any = window as any
 
 export const app = new Application({
 	view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
@@ -9,50 +10,75 @@ export const app = new Application({
 	autoDensity: true,
 	backgroundColor: 0x666666,
 	// backgroundAlpha: 0.001,
-	width: 1920,
-	height: 1080,
-}); global.app = app;
+	width: 640,
+	height: 360,
+})
+global.app = app
 
-const conty1: Container = new Container();
-conty1.x = 200;
-conty1.y = 200;
-app.stage.addChild(conty1);
+const container_1: Container = new Container()
+container_1.scale.set(0.5)
+container_1.position.set(app.screen.width / 2, app.screen.height / 2);
+app.stage.addChild(container_1)
 
-const conty2: Container = new Container();
-conty2.x = 500;
-conty2.y = 250;
-app.stage.addChild(conty2);
+const container_2: Container = new Container()
+container_2.position.set(500, 250)
+app.stage.addChild(container_2)
 
 // Load the shader program
-const vertexShader = vert;
-const fragmentShader = frag;
+const vertexShader = vert
+const fragmentShader = frag
 // const myShader = Shader.from(vertexShader, fragmentShader);
 
 // Create a custom filter
 const myFilter = new Filter(vertexShader, fragmentShader, {
 	uTintColor: [1, 0, 0, 1],
 	utime: 0,
-});
+})
 
 // Apply the filter to the sprite
-const sprite_1: Sprite = Sprite.from('mollu.gif');
-sprite_1.anchor.set(0.5);
-conty1.addChild(sprite_1);
-sprite_1.filters = [myFilter];
+const sprite_1: Sprite = Sprite.from('mollu.gif')
+sprite_1.anchor.set(0.5)
+container_1.addChild(sprite_1)
+sprite_1.filters = [myFilter]
 
-const sprite_2: Sprite = Sprite.from('hos.png');
-sprite_2.anchor.set(0.5);
-conty2.addChild(sprite_2);
+const sprite_2: Sprite = Sprite.from('hos.png')
+sprite_2.anchor.set(0.5)
+container_2.addChild(sprite_2)
 
-let time = 0;
+let time = 0
 app.ticker.add((delta) => {
-	time += delta;
-	let slow_time = time * 0.01;
+	time += delta
+	let slow_time = time * 0.01
 	// rotate the container!
 	// use delta to create frame-independent transform
 	// conty.rotation -= 0 * delta;
-	conty2.rotation -= 0.01 * delta;
+	container_2.rotation -= 0.01 * delta
 	// conty2.x = Math.abs(Math.sin(slow_time)) * 400;
-	myFilter.uniforms.uTintColor = [Math.sin(slow_time), 1 - Math.sin(slow_time), Math.sin(slow_time + 2), 1];
+	myFilter.uniforms.uTintColor = [Math.sin(slow_time), 1 - Math.sin(slow_time), Math.sin(slow_time + 2), 1]
 	// myFilter.uniforms.utime = Math.sin(slow_time) * 400
+})
+
+const graphics: Graphics = new Graphics();
+
+graphics.beginFill(0xFF00FF);
+graphics.lineStyle(10, 0x00FF00);
+graphics.drawCircle(0, 0, 25);
+graphics.endFill()
+
+app.stage.addChild(graphics);
+
+graphics.position.set(100, 100);
+
+
+const styly: TextStyle = new TextStyle({
+    align: 'left',
+    fill: "#999999",
+    fontSize: 42
 });
+
+// Text supports unicode!
+const texty: Text = new Text('몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n몰루? 몰루? 몰루? 몰루? \n', styly); 
+texty.position.set(100, 0)
+
+// texty.text = "This is expensive to change, please do not abuse";
+app.stage.addChild(texty);
