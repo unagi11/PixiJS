8c2e1c961f8f52b510d876475af46559436d1b44
Unagi11 <unagi11@naver.com>
2023-02-12 19:30:40 +0900

    build: 2023-02-12 19:30:40

---
+++ src/loader.ts
@@ -0,0 +1,3 @@
+import { Assets } from 'pixi.js';
+
+Assets.load('PF스타더스트.ttf')
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import frag from './shaders/fragment.frag'
 import vert from './shaders/vertex.vert'
-import { Application, Sprite, Container, Filter, Point, Graphics, Text, TextStyle } from 'pixi.js'
+import { Application, Sprite, Container, Filter, Point, Graphics, Text, TextStyle, BitmapText, Loader, loadWebFont, Assets } from 'pixi.js'
 
 export const global: any = window as any
 
@@ -45,18 +45,6 @@ const sprite_2: Sprite = Sprite.from('hos.png')
 sprite_2.anchor.set(0.5)
 container_2.addChild(sprite_2)
 
-let time = 0
-app.ticker.add((delta) => {
-	time += delta
-	let slow_time = time * 0.01
-	// rotate the container!
-	// use delta to create frame-independent transform
-	// conty.rotation -= 0 * delta;
-	container_2.rotation -= 0.01 * delta
-	// conty2.x = Math.abs(Math.sin(slow_time)) * 400;
-	myFilter.uniforms.uTintColor = [Math.sin(slow_time), 1 - Math.sin(slow_time), Math.sin(slow_time + 2), 1]
-	// myFilter.uniforms.utime = Math.sin(slow_time) * 400
-})
 
 const graphics: Graphics = new Graphics();
 
@@ -69,18 +57,48 @@ app.stage.addChild(graphics);
 
 graphics.position.set(100, 100);
 
-
 const styly: TextStyle = new TextStyle({
     align: 'left',
     fill: "#00CCFF",
     fontSize: 42
 });
 
-// Text supports unicode!
+const style = new TextStyle({
+    fontFamily: "PF스타더스트",
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+});
 
-let array = Array(10).fill('몰?루 몰?루 몰?루 몰?루\n').join('')
-const texty: Text = new Text(array, styly);  
+let str = ""
+const texty: Text = new Text(str, style); 
 texty.position.set(100, 0)
-
-// texty.text = "This is expensive to change, please do not abuse";
 app.stage.addChildAt(texty, 0);
+
+Assets.load('PF스타더스트.ttf')
+
+let time = 0
+app.ticker.add((delta) => {
+	time += delta
+	let slow_time = time * 0.01
+	// rotate the container!
+	// use delta to create frame-independent transform
+	// conty.rotation -= 0 * delta;
+	container_2.rotation -= 0.01 * delta
+
+    texty.text = `${time}\n${time}\n${time}\n${time}\n${time}\n${time}\n${time}`;
+	// conty2.x = Math.abs(Math.sin(slow_time)) * 400;
+	myFilter.uniforms.uTintColor = [Math.sin(slow_time), 1 - Math.sin(slow_time), Math.sin(slow_time + 2), 1]
+	// myFilter.uniforms.utime = Math.sin(slow_time) * 400
+})
