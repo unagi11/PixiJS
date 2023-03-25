40c3417b1a3b92e23ad70311120fb6d3634a109e
Unagi11 <unagi11@naver.com>
2023-03-25 21:40:39 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ export const app = new Application({
 	resolution: 1,
 	autoDensity: true,
 	backgroundColor: 0x666666,
-	width: window.innerWidth,
-	height: window.innerHeight,
+	width: global.DESIGN_WIDTH,
+	height: global.DESIGN_HEIGHT,
 	antialias: false,
 });
 global.app = app;
@@ -30,73 +30,20 @@ main();
 async function main() {
 	await LOADER.load_all();
 
-	window.addEventListener('resize', () => {
-		let width = window.innerWidth;
-		let height = window.innerHeight;
-		app.renderer.resize(width, height);
-	});
+    function resize () {
+        let inner_ratio = window.innerWidth / window.innerHeight;
+        let design_ratio = global.DESIGN_WIDTH / global.DESIGN_HEIGHT;
 
-    let texture = Texture.from('susino');
-    let ss = new SliceableSprite(app.stage , texture);
+        if (inner_ratio > design_ratio) { // 윈도우 width가 더 긴경우.
+            app.renderer.resize(window.innerHeight / 9 * 16, window.innerHeight);
+        } 
+        else { // 윈도우 height가 더 긴경우
+            app.renderer.resize(window.innerWidth, window.innerWidth / 16 * 9);
+        }
+    }
 
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(30, {x : 100, y : 0}));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(150, {x : 100, y : 0}));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(60, {x : 100, y : 0}));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(90, {x : 100, y : 0}));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(120, {x : 100, y : 0}));
-    // ss.draw();
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(180, {x : 100, y : 0}));
+    window.onresize = resize;
+    resize();
     
 
-    // range(0, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: 150, y: 0 }));
-	// });
-
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(0, { x: 150, y: 0 }));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(30, { x: 150, y: 0 }));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(60, { x: 150, y: 0 }));
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(90, { x: 150, y: 0 })); 
-    // ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(120, { x: 150, y: 0 }));
-		// ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(150, { x: 150, y: 0 }));
-
-	// range(0, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: -200, y: 0 }));
-	// });
-
-	// range(, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: -200, y: 0 }));
-	// });
-
-
-	// range(0, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: +200, y: 0 }));
-	// });
-    
-	// range(0, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: 0, y: 200 }));
-	// });
-
-	// range(0, 180, 30).forEach((degree) => {
-	// 	ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(degree, { x: 0, y: -200 }));
-	// });
-
-    range(-ss.rect.width , ss.rect.width , ss.rect.width / 20).forEach(x => {
-        ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(60,{x , y : 0}));
-    })
-
-    range(-ss.rect.width , ss.rect.width , ss.rect.width / 20).forEach(x => {
-        ss.polygons = ss.sliceByLine(ss.polygons, Slice.getDegreeLine(120,{x , y : 0}));
-    })
-
-    // ss.polygons = ss.sliceByLine(ss.polygons, 
-    //     Slice.get2DotLine({x : 0, y: -ss.rect.height / 2},{x : 1 , y: -ss.rect.height / 2}));
-    // ss.polygons = ss.sliceByLine(ss.polygons, 
-    //     Slice.get2DotLine({x : 0, y: -ss.rect.height / 2 + ss.rect.height / 10 * 9},
-    //     {x : 1 , y: -ss.rect.height / 2 + ss.rect.height / 10 * 9}));
-    
-    // range(-ss.rect.height / 2, ss.rect.height / 2, ss.rect.height / 10).forEach(y => {
-    // }) 
-
-    ss.draw(true);
-    ss.root.position.set(400, 550);
 }
