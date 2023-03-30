ad53b5d381e8c71583cba293361ebf432759805c
Unagi11 <unagi11@naver.com>
2023-03-30 14:16:36 +0900

    build: 2023-03-30 14:16:36

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import F_hologram from './shader/hologram.frag';
 import * as LOADER from './loader';
-import { Application, Container, Filter, Graphics, Point, Sprite, TextStyle, Texture, TEXT_GRADIENT } from 'pixi.js';
+import { Application, Container, Filter, Graphics, Point, Sprite, TextStyle, Texture, TextureUvs, TEXT_GRADIENT } from 'pixi.js';
 import { SliceableSprite , Slice, range} from './slice'
 
 
@@ -21,6 +21,9 @@ global.app = app;
 global.DESIGN_WIDTH = 1280;
 global.DESIGN_HEIGHT = 720;
 
+global.root = new Container();
+app.stage.addChild(global.root);
+
 // main 함수 실행
 main();
 
@@ -35,15 +38,24 @@ async function main() {
         let design_ratio = global.DESIGN_WIDTH / global.DESIGN_HEIGHT;
 
         if (inner_ratio > design_ratio) { // 윈도우 width가 더 긴경우.
+            let scale = window.innerHeight / global.DESIGN_HEIGHT;
+            (<Container>global.root).transform.scale.set(scale);
             app.renderer.resize(window.innerHeight / 9 * 16, window.innerHeight);
         } 
         else { // 윈도우 height가 더 긴경우
+            let scale = window.innerWidth / global.DESIGN_WIDTH;
+            (<Container>global.root).transform.scale.set(scale);
             app.renderer.resize(window.innerWidth, window.innerWidth / 16 * 9);
         }
     }
 
+    let background = Sprite.from('background.png');
+    let box = Sprite.from('box.png')
+
+
+    global.root.addChild(background);
+    global.root.addChild(box);
+
     window.onresize = resize;
     resize();
-    
-
 }
