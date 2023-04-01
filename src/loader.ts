756fa1b30d34b476395f66e0fb9bb5096ff2b996
Unagi11 <unagi11@naver.com>
2023-04-02 00:19:05 +0900

    build: 2023-04-02  0:19:05

--- src/loader.ts
+++ src/loader.ts
@@ -5,8 +5,8 @@ import asset_option from './json/asset.json';
  * 에셋을 로드 하는 함수.
  */
 export async function load_all() {
-	await Assets.init({ manifest: asset_option });
-	await Assets.loadBundle('test-screen');
+	// await Assets.init({ manifest: asset_option });
+	// await Assets.loadBundle('test-screen');
 	// let loadScreenAssets = await Assets.loadBundle('load-screen');
 	// let gameScreenAssets = await Assets.loadBundle('game-screen')
 
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as LOADER from './loader';
-import { Application, Container, Filter, Graphics, Sprite, TextStyle, Texture, TextureUvs, TEXT_GRADIENT } from 'pixi.js';
-import { SliceableSprite , Slice, range} from './slice'
-
+import * as PIXI from 'pixi.js';
+import Scene1_json from '../static/ase/Scene1.json';
 
 /**
  * pixi application 생성
  */
-export const app = new Application({
+export const app = new PIXI.Application({
 	view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
 	resolution: 1,
 	autoDensity: true,
-	backgroundColor: 0x000000,
+	backgroundColor: 0x111111,
 	width: global.DESIGN_WIDTH,
 	height: global.DESIGN_HEIGHT,
 	antialias: false,
@@ -23,7 +22,7 @@ global.DESIGN_HEIGHT = 720;
 global.LETTER_WIDTH = 0;
 global.LETTER_HEIGHT = 0;
 
-global.root = new Container();
+global.root = new PIXI.Container();
 app.stage.addChild(global.root);
 
 // main 함수 실행
@@ -38,7 +37,7 @@ async function main() {
     function resize () {
         let inner_ratio = window.innerWidth / window.innerHeight;
         let design_ratio = global.DESIGN_WIDTH / global.DESIGN_HEIGHT;
-        let root = global.root as Container;
+        let root = global.root as PIXI.Container;
         
         // 윈도우 width가 더 긴경우
         if (inner_ratio > design_ratio) {
@@ -62,12 +61,58 @@ async function main() {
         }
     }
 
-    let background = Sprite.from('background.png');
-    let box = Sprite.from('box.png')
-
-    global.root.addChild(background);
-    global.root.addChild(box);
-
     window.onresize = resize;
     resize();
+
+    MakeScene(Scene1_json);
+}
+
+function MakeScene(scene_data : any) {
+	let scene1_frames = scene_data.frames;
+    let keys : string[] = Object.keys(scene1_frames);
+    let values : Frame[] = Object.values(scene1_frames);
+
+    values.map((value : Frame, index : number) : [string, Frame] => {
+        return [keys[index], value];
+    }).forEach((value : [string, Frame]) => {
+        let name : string = value[0];
+        let frame : Frame = value[1];
+
+        let texture = PIXI.BaseTexture.from('ase/Scene1.png');
+        let rect =  new PIXI.Rectangle(frame.frame.x, frame.frame.y, frame.frame.w, frame.frame.h);
+        let trimmed_texture = new PIXI.Texture(texture, rect);
+
+        // 이미지의 일부분을 잘라내어 Sprite 생성
+        const sprite = new PIXI.Sprite(trimmed_texture);
+        sprite.transform.position.set(frame.spriteSourceSize.x, frame.spriteSourceSize.y);
+        sprite.name = name;
+        
+        // add touch button on sprite
+        sprite.interactive = true;
+        sprite.on('pointerdown', (a) => {
+            console.log('clicked !! ' + sprite.name);
+        });
+
+        // 스테이지에 Sprite 추가
+        global.root.addChild(sprite);
+    });
+}
+
+interface Frame {
+	frame: {
+		x: number;
+		y: number;
+		w: number;
+		h: number;
+	};
+	spriteSourceSize: {
+		x: number;
+		y: number;
+		w: number;
+		h: number;
+	};
+	soruceSize: {
+		w: number;
+		h: number;
+	};
 }
