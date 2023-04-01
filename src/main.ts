1b9d870865e49153ed9060d7c96903c4e58b6ab0
Unagi11 <unagi11@naver.com>
2023-04-01 21:28:00 +0900

    build: 2023-04-01 21:28:00

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import F_hologram from './shader/hologram.frag';
 import * as LOADER from './loader';
-import { Application, Container, Filter, Graphics, Point, Sprite, TextStyle, Texture, TextureUvs, TEXT_GRADIENT } from 'pixi.js';
+import { Application, Container, Filter, Graphics, Sprite, TextStyle, Texture, TextureUvs, TEXT_GRADIENT } from 'pixi.js';
 import { SliceableSprite , Slice, range} from './slice'
 
 
@@ -11,7 +10,7 @@ export const app = new Application({
 	view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
 	resolution: 1,
 	autoDensity: true,
-	backgroundColor: 0x666666,
+	backgroundColor: 0x000000,
 	width: global.DESIGN_WIDTH,
 	height: global.DESIGN_HEIGHT,
 	antialias: false,
@@ -20,6 +19,9 @@ global.app = app;
 
 global.DESIGN_WIDTH = 1280;
 global.DESIGN_HEIGHT = 720;
+
+global.LETTER_WIDTH = 0;
+global.LETTER_HEIGHT = 0;
 
 global.root = new Container();
 app.stage.addChild(global.root);
@@ -36,22 +38,32 @@ async function main() {
     function resize () {
         let inner_ratio = window.innerWidth / window.innerHeight;
         let design_ratio = global.DESIGN_WIDTH / global.DESIGN_HEIGHT;
-
-        if (inner_ratio > design_ratio) { // 윈도우 width가 더 긴경우.
+        let root = global.root as Container;
+        
+        // 윈도우 width가 더 긴경우
+        if (inner_ratio > design_ratio) {
             let scale = window.innerHeight / global.DESIGN_HEIGHT;
-            (<Container>global.root).transform.scale.set(scale);
-            app.renderer.resize(window.innerHeight / 9 * 16, window.innerHeight);
+            global.LETTER_WIDTH = window.innerWidth / 2 - (window.innerHeight / 9) * 16 / 2;
+            global.LETTER_HEIGHT = 0; 
+
+            root.transform.scale.set(scale);
+            root.transform.position.set(global.LETTER_WIDTH, global.LETTER_HEIGHT);
+            app.renderer.resize(window.innerWidth, window.innerHeight);
         } 
-        else { // 윈도우 height가 더 긴경우
+        // 윈도우 height가 더 긴경우
+        else {
             let scale = window.innerWidth / global.DESIGN_WIDTH;
-            (<Container>global.root).transform.scale.set(scale);
-            app.renderer.resize(window.innerWidth, window.innerWidth / 16 * 9);
+            global.LETTER_WIDTH = 0;
+            global.LETTER_HEIGHT = window.innerHeight / 2 - (window.innerWidth / 16) * 9/ 2;
+
+            root.transform.scale.set(scale);
+            root.transform.position.set(global.LETTER_WIDTH, global.LETTER_HEIGHT);
+            app.renderer.resize(window.innerWidth, window.innerHeight);
         }
     }
 
     let background = Sprite.from('background.png');
     let box = Sprite.from('box.png')
-
 
     global.root.addChild(background);
     global.root.addChild(box);
